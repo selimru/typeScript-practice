0e1231a9c82598350cbbea39288404406f5c8352
selimRU <selim.soha92@gmail.com>
2024-01-03 15:40:28 +0600

    typescript practice object type

--- various-type/object-type.js
+++ various-type/object-type.js
@@ -1,5 +1,6 @@
 var users;
+users = [];
 var user1;
 user1 = { name: 'saihan', age: 5, id: 1 };
-users = [user1];
+users.push(user1);
 console.log(users);
--- various-type/object-type.ts
+++ various-type/object-type.ts
@@ -1,6 +1,7 @@
 let users: object[]
+users = []
 let user1: { name: string, age: number, id: number };
 user1 = { name: 'saihan', age: 5, id: 1 }
-users = [user1]
+users.push(user1)
 
 console.log(users);
